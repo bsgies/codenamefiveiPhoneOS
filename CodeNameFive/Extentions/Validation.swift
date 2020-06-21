38b82ec4c27da742e728af4e1b623766e223c587
muhammadimran <imranrasheed.developer@outlook.com>
2020-06-21 23:29:59 +0500

    Validation

---
+++ CodeNameFive/Extentions/Validation.swift
@@ -0,0 +1,26 @@
+//
+//  Validation.swift
+//  CodeNameFive
+//
+//  Created by Muhammad Imran on 20/06/2020.
+//  Copyright © 2020 ITRID TECHNOLOGIES LTD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String
+{
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    func isValidPhone(phone: String) -> Bool {
+        let phoneRegex = "^[0-9+]{0,1}+[0-9]{5,16}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        return phoneTest.evaluate(with: phone)
+    }
+}
