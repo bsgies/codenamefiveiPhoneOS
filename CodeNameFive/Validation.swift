a38669a99d762dd162710e31e8d1a80fb581bd37
muhammadimran <imranrasheed.developer@outlook.com>
2020-06-21 23:44:44 +0500

    Validate

---
+++ CodeNameFive/Validation.swift
@@ -0,0 +1,9 @@
+//
+//  Validation.swift
+//  CodeNameFive
+//
+//  Created by Muhammad Imran on 20/06/2020.
+//  Copyright © 2020 ITRID TECHNOLOGIES LTD. All rights reserved.
+//
+
+import Foundation
