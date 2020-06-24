6d85b1c5167b3ad233e8d686cab76b28c6062b57
muhammadimran <imranrasheed.developer@outlook.com>
2020-06-25 01:35:51 +0500

    Profile-TripHistory

---
+++ CodeNameFive/Controller/WeeklyTripsDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeeklyTripsDataViewController.swift
+//  CodeNameFive
+//
+//  Created by Muhammad Imran on 25/06/2020.
+//  Copyright © 2020 ITRID TECHNOLOGIES LTD. All rights reserved.
+//
+
+import UIKit
+
+class WeeklyTripsDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
