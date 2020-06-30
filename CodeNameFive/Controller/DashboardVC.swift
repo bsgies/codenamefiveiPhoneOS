bdce4fd2d55b6acb5000e72b2e35b6238a500be5
muhammadimran <imranrasheed.developer@outlook.com>
2020-07-01 00:38:53 +0500

    Maps

--- CodeNameFive/Controller/DashboardVC.swift
+++ CodeNameFive/Controller/DashboardVC.swift
@@ -10,7 +10,8 @@ import UIKit
 import MapKit
 import CoreLocation
 class DashboardVC: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
-
+    @IBOutlet weak var addressLbl: UILabel!
+    var address = ""
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var menuButton: UIButton!
     
@@ -19,9 +20,6 @@ class DashboardVC: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setNeedsStatusBarAppearanceUpdate()
-//        let statusBarColor = UIColor(ciColor: .clear)
-//        navigationController?.setStatusBar(backgroundColor: statusBarColor)
         menuButton.layer.cornerRadius = menuButton.frame.size.width / 2
         menuButton.layer.shadowColor = UIColor(ciColor: .gray).cgColor
         menuButton.layer.shadowRadius = 1
@@ -30,12 +28,19 @@ class DashboardVC: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate 
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-
-        // Check for Location Services
+        
         if CLLocationManager.locationServicesEnabled() {
+            guard let currentLocation = locationManager.location else {
+                return
+            }
             locationManager.requestWhenInUseAuthorization()
             locationManager.startUpdatingLocation()
         }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+            print("\(self.currentLocation!.coordinate.latitude) and \(String(describing: self.currentLocation?.coordinate.longitude))")
+                       self.userAddres(Latitude: self.currentLocation!.coordinate.latitude, withLongitude: (self.currentLocation?.coordinate.longitude)!)
+        })
     }
     
     @IBAction func MenuButtonAction(_ sender: Any) {
@@ -65,6 +70,47 @@ class DashboardVC: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate 
                 mapView.setRegion(viewRegion, animated: false)
             }
         }
+    }
+    
+    func userAddres(Latitude: Double, withLongitude Longitude: Double) {
+        var center : CLLocationCoordinate2D = CLLocationCoordinate2D()
+        let lat: Double = Latitude
+        let lon: Double = Longitude
+        let geocoder: CLGeocoder = CLGeocoder()
+        center.latitude = lat
+        center.longitude = lon
+
+        let loc: CLLocation = CLLocation(latitude:center.latitude, longitude: center.longitude)
+
+
+        geocoder.reverseGeocodeLocation(loc, completionHandler:
+            {(placemarks, error) in
+                if (error != nil)
+                {
+                    print(error!.localizedDescription)
+                }
+                let pm = placemarks! as [CLPlacemark]
+                if pm.count > 0 {
+                    let placeMark = placemarks![0]
+                  
+                    if placeMark.subLocality != nil {
+                        self.address = self.address + placeMark.subLocality! + ", "
+                    }
+                    if placeMark.thoroughfare != nil {
+                        self.address = self.address + placeMark.thoroughfare! + ", "
+                    }
+                    if placeMark.locality != nil {
+                        self.address = self.address + placeMark.locality! + ", "
+                    }
+                    if placeMark.country != nil {
+                        self.address = self.address + placeMark.country! + ", "
+                    }
+ 
+                    self.addressLbl.text = self.address
+                    print(self.address)
+              }
+        })
+
     }
 
 }
