01cc4d7dd786a43feb0557ac5cb665babb5d936c
muhammadimran <imranrasheed.developer@outlook.com>
2020-07-05 15:01:25 +0500

    add time in message box

--- CodeNameFive/Cell/ChatCell.swift
+++ CodeNameFive/Cell/ChatCell.swift
@@ -11,8 +11,9 @@ import UIKit
 class ChatCell: UITableViewCell {
 
   let messageLabel = UILabel()
-        let bubbleBackgroundView = UIView()
-        
+  let bubbleBackgroundView = UIView()
+  let time = UILabel()
+  let status = UILabel()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
             super.init(style: style, reuseIdentifier: reuseIdentifier)
             
@@ -21,10 +22,16 @@ class ChatCell: UITableViewCell {
             bubbleBackgroundView.translatesAutoresizingMaskIntoConstraints = false
             addSubview(bubbleBackgroundView)
             addSubview(messageLabel)
+            addSubview(time)
+            addSubview(status)
             messageLabel.numberOfLines = 0
             
             messageLabel.translatesAutoresizingMaskIntoConstraints = false
-            
+            time.translatesAutoresizingMaskIntoConstraints = false
+            time.text = "21:21"
+        status.text = "sent"
+        //time.font = UIFont(name: "Poppins-Light", size: 9.0)
+        time.font = UIFont.systemFont(ofSize: 11.0)
             // lets set up some constraints for our label
             let constraints = [messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
             //messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
@@ -32,10 +39,21 @@ class ChatCell: UITableViewCell {
             messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
             messageLabel.widthAnchor.constraint(equalToConstant: 250),
             
+            
             bubbleBackgroundView.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: -16),
             bubbleBackgroundView.leadingAnchor.constraint(equalTo: messageLabel.leadingAnchor, constant: -16),
             bubbleBackgroundView.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 16),
             bubbleBackgroundView.trailingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 16),
+            
+            
+            time.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 10),
+            
+            time.trailingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 10),
+            
+            
+            
+            
+            
             ]
             NSLayoutConstraint.activate(constraints)
         }
