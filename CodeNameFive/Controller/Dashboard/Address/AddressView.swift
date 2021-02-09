c749449c1ff0d433bfc8a9416d628e54e4ef0c4d
muhammadimran <imranrasheed.developer@outlook.com>
2021-02-10 02:58:19 +0500

    new updates

---
+++ CodeNameFive/Controller/Dashboard/Address/AddressView.swift
@@ -0,0 +1,21 @@
+//
+//  AddressView.swift
+//  CodeNameFive
+//
+//  Created by Muhammad Imran on 09/02/2021.
+//  Copyright © 2021 ITRID TECHNOLOGIES LTD. All rights reserved.
+//
+
+import UIKit
+
+class AddressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
